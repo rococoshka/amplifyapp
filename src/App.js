d7ae0d4433a7b965e35f8bd818b187677e74f5db
Aleh Kurcheuski <aleh.kurcheuski@innowise-group.com>
2021-06-24 16:42:40 +0300

    page for Aleksey

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Aliona, it's React app was deployed by Aws aPLIFY when I push to git hub.
+          Hello Alexey, it's React app was deployed by Aws aPLIFY when I push to git hub.
         </p>
+	<p>
+	  So if you need something like this shit you know my phone number:D
         <a
           className="App-link"
           href="https://reactjs.org"
